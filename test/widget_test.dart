a3beed96543c3c916fd502b8f85d3b0b1dc6870c
AngelS1708 <a327763@alumnos.uaslp.mx>
2023-05-31 16:00:12 -0600

    Version Final hecho con FF

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ggproyect/main.dart';
+import 'package:g_gproyect/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
